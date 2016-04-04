1462a062cf870b568db3ac0584e9d6ce7daf7bd0
Giteje <zhuangv@saic.com>
2016-04-04 16:52:14 -0400

    Updated widget for HTTPS compatibility and latest WAB version
    Updated widget for HTTPS compatibility and latest WAB version

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # SearchEFbyState-widget-js (OEI)
 
 ## Updates
+April 4, 2016
+Updated widget for HTTPS compatibility and latest WAB version
+
 October 7, 2015
 Updated widget to be compatible with corresponding HTML widget
 
--- SearchEFbyState/EFdownload.js
+++ SearchEFbyState/EFdownload.js
@@ -1,11 +1,22 @@
 define([
-    "dojo/_base/declare",
-    "dijit/_Widget",
+  "dojo/_base/declare",
+  "dojo/dom-construct",
+  "dijit/_Widget",
   "dijit/_Templated",
   "dojo/text!./templates/EFdownload.html",
   "esri/tasks/Geoprocessor",
+  "esri/geometry/webMercatorUtils",
   "./configLocal"
-], function(declare, _Widget,_Templated,template,Geoprocessor,_config){
+], function(
+    declare,   
+    domConstruct,
+    _Widget,
+    _Templated,
+    template,
+    Geoprocessor,
+    webMercatorUtils,
+    _config
+    ){
     return declare("EFdownload", [_Widget, _Templated], {
     templateString: template,
     widgetsInTemplate: false,
@@ -19,59 +30,42 @@ define([
         this.eftype = options.eftype;
         this.parentWidget = options.parentWidget;
         // mixin constructor options 
-        dojo.safeMixin(this, options);
+        declare.safeMixin(this, options);
 
-
-
-//move to config
        var maprestbaseurl = _config.maprestbaseurl; 
        var gpxmlclusterurl = maprestbaseurl + _config.gpxmlclusterurl;        
        var gpxml2shpcsvurl = maprestbaseurl + _config.gpxml2shpcsvurl;
-       var efoasurl = _config.efsearchurl;
+       var efoasurl = _config.efsearchurl;       
        var re = /^.*\/(.*)\/$/;
        var tagname = efoasurl.replace(re, "$1"); 
 
         this.gpconfig = {
-    "location": { "featureurl": gpxmlclusterurl, "shpcsvurl": gpxml2shpcsvurl,"maintag": tagname }
-    };    
+             "location": { "featureurl": gpxmlclusterurl, "shpcsvurl": gpxml2shpcsvurl,"maintag": tagname }
+        };    
 
-        var gpshpurl = this.gpconfig[this.eftype].shpcsvurl;
-        //this.gp = new esri.tasks.Geoprocessor(gpshpurl);
+        var gpshpurl = this.gpconfig[this.eftype].shpcsvurl;        
         this.gp = new Geoprocessor(gpshpurl);
-
     },
-
     startup: function () {
-
-
     },
     postCreate: function () {
        this.imgSHP.src = this.parentWidget.folderUrl + "images/shpBtn.png";
        this.imgCSV.src = this.parentWidget.folderUrl + "images/csvBtn.png";
        this.imgKML.src = this.parentWidget.folderUrl + "images/geBtn.png";       
     },
-
     _saveShapefile: function () {       
 
-
         this.loadNode.innerHTML = "Loading... please wait";
-
-        //var efbaseurl = this.inefbaseurl;
+      
         var params;
-        //var efbaseurl;
-        //var efsearchurl = "http://oaspub.epa.gov/enviro/efservice/mv_new_geo_best_picks/"; //search EF by location
-        //var searchtext = "22153";
-        //
         var efbaseurl = this.parentWidget.baseurl;
         var maintagname = this.gpconfig[this.eftype].maintag;
 
-        if (this.eftype == "location") {
-            //var efbaseurl = dijit.byId("efwg").baseurl;
+        if (this.eftype == "location") {           
             params = { "xmlurl": efbaseurl, "main_tagname": maintagname, "latitude_tagname": "LATITUDE", "longitude_tagname": "LONGITUDE", "output_format": "shapefile" };
-        } else {
-            //var efbaseurl = dijit.byId("efwg").baseurl;
+        } else {           
             var ext = this.map.extent;
-            var mextent = esri.geometry.webMercatorToGeographic(ext);
+            var mextent = webMercatorUtils.webMercatorToGeographic(ext);
             params = { "xmlurl": efbaseurl, "min_longitude": mextent.xmin, "min_latitude": mextent.ymin, "max_longitude": mextent.xmax, "max_latitude": mextent.ymax, "output_format": "shapefile" };
         }
         if (this.gprequest) this.gprequest.cancel();
@@ -89,18 +83,15 @@ define([
         this.loadNode.innerHTML = "Loading... please wait";
         if (this.gprequest) this.gprequest.cancel();
         
-
         var params;
         var efbaseurl = this.parentWidget.baseurl;
         var maintagname = this.gpconfig[this.eftype].maintag;
 
-        if (this.eftype == "location") {
-            
+        if (this.eftype == "location") {            
             params = { "xmlurl": efbaseurl, "main_tagname": maintagname, "latitude_tagname": "LATITUDE", "longitude_tagname": "LONGITUDE", "output_format": "csvfile" };
-        } else {
-            
+        } else {            
             var ext = this.map.extent;
-            var mextent = esri.geometry.webMercatorToGeographic(ext);
+            var mextent = webMercatorUtils.webMercatorToGeographic(ext);
             params = { "xmlurl": efbaseurl, "min_longitude": mextent.xmin, "min_latitude": mextent.ymin, "max_longitude": mextent.xmax, "max_latitude": mextent.ymax, "output_format": "csvfile" };
         }
         var wobj = this;
@@ -112,32 +103,24 @@ define([
             wobj.loadNode.innerHTML = "Generate CSV file failed. Error: " + err;
             console.log("CSV gp failed: ", err);
         });
-    },
-    _saveGeoRss: function () {
-
-    },
+    },   
     _saveKML: function () {
         var gpfeaturl = this.gpconfig[this.eftype].featureurl;
         var maintagname = this.gpconfig[this.eftype].maintag;
 
         var url = "";
         var efbaseurl = this.parentWidget.baseurl;
-        if (this.eftype == "location") {
-            //var efbaseurl = dijit.byId("efwg").baseurl;
+        if (this.eftype == "location") {            
             url = gpfeaturl + "/execute?xmlurl=" + encodeURIComponent(efbaseurl) + "&main_tagname=" + maintagname + "&latitude_tagname=LATITUDE&longitude_tagname=LONGITUDE&showcluster=false&f=kmz";
-        } else {
-            //var efbaseurl = dijit.byId("efwg").baseurl;
+        } else {            
             var ext = this.map.extent;
-            var mextent = esri.geometry.webMercatorToGeographic(ext);
+            var mextent = webMercatorUtils.webMercatorToGeographic(ext);
             url = gpfeaturl + "/execute?xmlurl=" + encodeURIComponent(efbaseurl) + "&min_longitude=" + mextent.xmin + "&min_latitude=" + mextent.ymin + "&max_longitude=" + mextent.xmax + "&max_latitude=" + mextent.ymax + "&showcluster=false&f=kmz";
         }
         window.open(url);
     },
-
     destroy: function () {
-
-        dojo.empty(this.domNode);
-
+        domConstruct.empty(this.domNode);
     }
  });
 
